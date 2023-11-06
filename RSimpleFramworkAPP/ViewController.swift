786b993305b8bb9eb991279dfe55de6b95832f26
Rajesh <rajesh.s@vrinsoft.com>
2023-11-06 12:48:01 +0530

    Initial Commit

---
+++ RSimpleFramworkAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSimpleFramworkAPP
+//
+//  Created by vrinsoft on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
